2d7d1cd41c7f849577259ce7d376c003ca6f7c97
Itachi1101 <aviral1101@gmail.com>
2020-10-29 19:52:34 +0530

    CP_optimised

---
+++ Undirected-Graphs/Connected_Components_CP-algorithms.cpp
@@ -0,0 +1,27 @@
+int n;
+vector<int>g[MAXN];
+bool used[MAXN];
+vector<int>comp;
+void dfs(int v) {
+	used[v] = true;
+	comp.push_back(v);
+	for (size_t i = 0; i < (int)g[v].size(); ++i) {
+		int to = g[v][i];
+		if (!used[to])
+			dfs(to);
+	}
+}
+void find_comps() {
+	used.assign(n, false);
+	for (int i = 0; i < n; ++i) {
+		if (!used[i]) {
+			comp.clear();
+			dfs(i);
+			court << "connected:";
+			for (size_t j = 0; j < comp.size(); ++j) {
+				cout << " " << comp[j];
+
+			} cout << endl;
+		}
+	}
+}
